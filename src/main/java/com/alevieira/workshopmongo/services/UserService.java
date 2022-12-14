f017bfae4e42c301db9a3a01c051ed591cd79802
ale-vieira <alexandrevieira76@gmail.com>
2022-12-14 15:03:54 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/alevieira/workshopmongo/services/UserService.java
@@ -0,0 +1,20 @@
+package com.alevieira.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alevieira.workshopmongo.domain.User;
+import com.alevieira.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+		
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll(){
+		return repo.findAll();
+	}
+}
